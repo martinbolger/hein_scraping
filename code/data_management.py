b5608b6583295cbd6b62a7f36809d96af6c46573
martinbolger <56566763+martinbolger@users.noreply.github.com>
2020-12-19 09:27:01 -0800

    [Modify] Adds a data subset to data_management.

--- code/data_management.py
+++ code/data_management.py
@@ -30,5 +30,20 @@ __date__ = "December 19th, 2020"
 # Create the paths for the data directories
 input_path, work_path, intr_path, out_path = create_path()
 
+# Import the dataset
+data = pd.read_excel(input_path / "prof_data_lateral_control.xlsx")
+
+# DATA CHECK: The lateral column should = 1 or 0 (for whether or
+# not each observation is a lateral). We want to make sure it 
+# doesn't take any other values.
+data_types = set(data["Lateral"].to_list())
+
+if not (1 in data_types and 0 in data_types and len(data_types) == 2):
+    print("ERROR: The variable Lateral contains unexpected values. Ending")
+    quit()
+
+# SUBSET: Subset the data into two datasets: the lateral and control groups
+control = data[data["Lateral"] == 0]
+lateral = data[data["Lateral"] == 1]
 
 
