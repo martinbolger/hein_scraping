c83caa2d9cb9cc723aeb4a4bb005ead05daafc74
martinbolger <56566763+martinbolger@users.noreply.github.com>
2020-12-20 10:01:45 -0800

    [Modify] Adds concat_function to data_manipulation_functions.

--- code/modules/data_manipulation_functions.py
+++ code/modules/data_manipulation_functions.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import pandas as pd
+
 #Removes commas from all values in a row of a dataframe
 def remove_commas(df1):
     for col in df1.columns:
@@ -16,8 +18,24 @@ def check_files(fm_name, last_name, current_files):
             break
     return done
 
+# This function converts a list of strings to a comma
+# separated string.
 def list_to_comma_separated_string(list_of_strings):
     list_str = ', '.join(list_of_strings)
     list_str = list_str.replace('\'', '')
     list_str.replace('\"', '')
     return list_str
+
+# This function is used to concatenate strings
+# in a panda dataframe. It can handle cases when
+# one of the strings is missing. It is used in an 
+# apply statement.
+def concat_function(x, y):
+    if not pd.isna(x) and not pd.isna(y):
+        return x + ", " + y
+    elif not pd.isna(x):
+        return x
+    elif not pd.isna(y):
+        return y
+    else:
+        return x
