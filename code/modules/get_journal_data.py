eeec014a5e2ffe20161ceb6fae66d2c3cf84a28e
martinbolger <56566763+martinbolger@users.noreply.github.com>
2021-01-01 15:09:46 -0800

    [Modify] Updates the way the get_journal_data extracts the volume, issue, and pages.

--- code/modules/get_journal_data.py
+++ code/modules/get_journal_data.py
@@ -1,28 +1,11 @@
 import pandas as pd
+import re
 
 def get_journal_data(in_df):
-    jname_series = in_df["Journal"]
-    index_series = in_df["ID"].reset_index(drop = True)
-    # Create the output df
-    df = pd.DataFrame(columns = ['ID', 'Journal Name', 'Vol', 'Issue', 'Pages'])
-    index = 0
-    for jname_string in jname_series:
-        data_stream = dict.fromkeys(['ID', 'Journal Name', 'Vol', 'Issue', 'Pages'], '')
-        if jname_string != '':
-            data_stream["ID"] = index_series.iloc[index]
-            data_stream['Journal Name'] = jname_string.split(', Vol.', 1)[0]
-            jelements = 'Vol. ' + jname_string.split('Vol.', 1)[1]
-            jelement_list = jelements.split(',')
-
-            for elm in jelement_list:
-                if 'Vol.' in elm:
-                    data_stream['Vol'] = elm.split('Vol.')[1].strip()
-                elif 'Issues' in elm:
-                    data_stream['Issue'] = elm.split('Issues')[1].strip()
-                elif 'Issue' in elm:
-                    data_stream['Issue'] = elm.split('Issue')[1].strip()
-                elif 'pp.' in elm:
-                    data_stream['Pages'] = elm.split('pp.')[1].strip()
-        df = df.append(pd.DataFrame([list(data_stream.values())], columns = ["ID", 'Journal Name', 'Vol', 'Issue', 'Pages']), sort=False)
-        index += 1
-    return df
+    in_df["Journal Name"] = in_df["Journal"].apply(lambda x: x.split(', Vol.', 1)[0] if x != '' else '')
+    vol_list = in_df["Journal"].apply(lambda x: re.search(r"Vol. (\d*-?\d*)", x))
+    in_df["Vol"] = vol_list.apply(lambda x: x.group(1) if x else '')
+    issue_list = in_df["Journal"].apply(lambda x: re.search(r"Issues? (\d*-?\d* \(.*\))", x))
+    in_df["Issue"] = issue_list.apply(lambda x: x.group(1) if x else '')
+    in_df['Pages'] = in_df["Journal"].apply(lambda x: x.split('pp.')[1].strip() if x != '' else '')
+    return in_df
