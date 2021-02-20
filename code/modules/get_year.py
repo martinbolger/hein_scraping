3135a737ad59ab73c8631a14055fdd783ca5e430
martinbolger <56566763+martinbolger@users.noreply.github.com>
2021-02-20 14:37:20 -0800

    [Add] Adds a get_year function for extracting years from strings.

---
+++ code/modules/get_year.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""get_year.py
+
+This module contains a function that tries to
+extract a four digit number from a string. If 
+there is no four digit number in the string,
+a blank is returned.
+"""
+
+import re
+
+__author__ = "Martin Bolger"
+__date__ = "December 19th, 2020"
+
+def get_year(string):
+    matches = re.search(r"\((\w* (\d*,\s)?)?((19|20)\d{2}-?((19|20)\d{2})?)\)", string)
+    if matches:
+        year = matches.group(3)
+    else:
+        year = ""
+    return year
+
+if __name__ == "__main__":
+    year = get_year("32 Envtl. L. Rep. News & Analysis 10003 (2002)")
+    print(year)
+    year = get_year("43 La. L. Rev. 1001 (1982-1983)")
+    print(year)
+    year = get_year("test string (")
+    print(year)
