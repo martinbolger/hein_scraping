512177be0449d9733ee572aa1a536ae25180b08b
martinbolger <56566763+martinbolger@users.noreply.github.com>
2021-02-28 11:29:32 -0800

    [Modify] Updates the get_year function.

--- code/modules/get_year.py
+++ code/modules/get_year.py
@@ -10,16 +10,18 @@ a blank is returned.
 import re
 
 __author__ = "Martin Bolger"
-__date__ = "December 19th, 2020"
+__date__ = "February 28th, 2021"
 
 def get_year(string):
     # Check to see if the string contains parentheses
     parentheses = re.search(r"[/(|/)]", string)
     if parentheses:
         # If it does, use the expression for matches inside of parentheses
-        matches = re.search(r"\((\w*((-|/)\w*)?,? (\d*,\s)?)?((19|20)\d{2}\s?-?\s?((19|20)?\d{2})?\s?)\)", string)
+        matches = re.search(r"\((\w*((-|/)\w*)?,? (\d*,\s)?)?((19|20)\d{2}\s?[-|,]?\s?((19|20)?\d{2})?\s?)\)", string)
         if matches:
             year = matches.group(5)
+            # Standardize the output for the year variable
+            year = re.sub(r"\s?[-|,]\s?", "-", year)
         else:
             year = ""
     else:
@@ -32,7 +34,7 @@ def get_year(string):
     return year
 
 if __name__ == "__main__":
-    test_cases = ["2004 Duke L. & Tech. Rev. 0009", "6 Int'l Fin. L. Rev. 13 (December, 1987)", "32 Envtl. L. Rep. News & Analysis 10003 (2002)", "43 La. L. Rev. 1001 (1982-1983)", "test string (", "5 (September-October 2003)", "1-2 (Winter/Spring 2003)", "5 (May 2010 )", "85 W. Va. L. Rev. 187 (1982-83)", "6 Buff. Crim. L. Rev. 1043 (2002 - 2003)", "2001 Fed. Cts. L. Rev. 1"]
+    test_cases = ["6 Tex. S. U. L. Rev. 7 (1979,1981)", "2004 Duke L. & Tech. Rev. 0009", "6 Int'l Fin. L. Rev. 13 (December, 1987)", "32 Envtl. L. Rep. News & Analysis 10003 (2002)", "43 La. L. Rev. 1001 (1982-1983)", "test string (", "5 (September-October 2003)", "1-2 (Winter/Spring 2003)", "5 (May 2010 )", "85 W. Va. L. Rev. 187 (1982-83)", "6 Buff. Crim. L. Rev. 1043 (2002 - 2003)", "2001 Fed. Cts. L. Rev. 1"]
     for case in test_cases:
         year = get_year(case)
         print("Test case: {}".format(case))
