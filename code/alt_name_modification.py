bbdb6ee8414d98a7b03bb71d98edcfc488d2062c
martinbolger <56566763+martinbolger@users.noreply.github.com>
2021-04-05 09:54:23 -0700

    [Modify] Adds a data_type variable to alt_name_modification.

--- code/alt_name_modification.py
+++ code/alt_name_modification.py
@@ -19,18 +19,21 @@ __date__ = "December 25th, 2020"
 # Create the paths for the data directories
 input_path, work_path, intr_path, out_path, selenium_driver_path = create_path()
 
+# Set the data type
+data_type = "control"
+
 # Load the name modification dataset
 # This is a dataframe of names that we want to manually change 
 # This can be used if we found errors in the data (for example, names were not scraped)
 # This dataset is edited by hand, so it is stored in the input directory
-name_mod = pd.read_excel(input_path / 'name_mod_lateral.xlsx')
+name_mod = pd.read_excel(input_path / 'name_mod_{}.xlsx'.format(data_type))
 
 # Load the full alt name dataset. This is the output of 
 # the alt name dataset creation script.
 alt_name_full = pd.read_excel(intr_path / "alt_names.xlsx")
 
 # MERGE: Merge on the name mod dataset
-alt_name_mod_name = pd.merge(alt_name_full, name_mod[["ID", "fm_names", "err_fm_names"]], how = "left", left_on = ["ID"], right_on = ["ID"], suffixes=('_orig', '_mod'))
+alt_name_mod_name = pd.merge(alt_name_full, name_mod[["ID", "fm_names", "err_fm_names", "alt_url"]], how = "left", left_on = ["ID"], right_on = ["ID"], suffixes=('_orig', '_mod'))
 
 # Concatenate the values from the alternate name and name mod dataframes
 alt_name_mod_name["fm_names"] = alt_name_mod_name.apply(lambda x: concat_function(x["fm_names_orig"], x["fm_names_mod"]), axis = 1) 
@@ -42,6 +45,9 @@ alt_name_mod_name["err_fm_names_mod"].fillna('', inplace=True)
 
 # Remove error names from the mod dataset from the list of names
 alt_name_mod_name["fm_names"] = alt_name_mod_name.apply(lambda x: remove_err_names(x["fm_names"], x["err_fm_names_mod"]), axis = 1)
+
+# Set the alt url column to 0 if it is blank
+alt_name_mod_name["alt_url"] = alt_name_mod_name["alt_url"].apply(lambda x: 0 if pd.isnull(x) else x).astype(int)
 
 alt_name_mod_name.to_excel(work_path / "alt_name_mod_name_full_vars.xlsx")
 
@@ -61,3 +67,5 @@ alt_name_mod_name["ID_counts"] = alt_name_mod_name.groupby(["ID"]).cumcount()+1
 
 # Output to Excel
 alt_name_mod_name.to_excel(intr_path / "hein_scraping_input_data.xlsx", index = False)
+
+print(alt_name_mod_name.head())
